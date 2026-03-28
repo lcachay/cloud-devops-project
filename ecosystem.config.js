2b101f128de68f44828174fb70bd6e319049d0a7
Lucia Cachay <lucia.cachayv@gmail.com>
2026-03-28 20:45:49 -0300

    pm2 use ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,11 +2,21 @@ module.exports = {
   apps: [
     {
       name: "cloud-devops-project",
-      script: "node",
-      args: "node_modules/next/dist/bin/next start -p 3000 -H 0.0.0.0",
+      script: "npm",
+      args: "start -- -p 3000 -H 0.0.0.0",
       watch: false,
+      cwd: "/opt/cloud-devops-project",
       env: {
+        NODE_ENV: "development",
+        ENVIRONMENT: "development",
+      },
+      env_development: {
+        NODE_ENV: "development",
+        ENVIRONMENT: "development",
+      },
+      env_production: {
         NODE_ENV: "production",
+        ENVIRONMENT: "production",
       },
     },
   ],
