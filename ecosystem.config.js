0daeee0d4ba4623656b9abacff61f75166f0e25f
Lucia Cachay <lucia.cachayv@gmail.com>
2026-03-28 22:17:37 -0300

    force fork exec_mode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,6 +6,7 @@ module.exports = {
       args: "start -p 3000 -H 0.0.0.0",
       watch: false,
       cwd: "/opt/cloud-devops-project",
+      exec_mode: "fork",
       env: {
         NODE_ENV: "production",
         ENVIRONMENT: "development",
