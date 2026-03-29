fbb6a804c43b9e6d53ab2ef4e1368feb707805b9
Lucia Cachay <lucia.cachayv@gmail.com>
2026-03-28 21:18:37 -0300

    will it work

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "cloud-devops-project",
-      script: "npm",
+      script: "node_modules/next/dist/bin/next",
       args: "start -- -p 3000 -H 0.0.0.0",
       watch: false,
       cwd: "/opt/cloud-devops-project",
